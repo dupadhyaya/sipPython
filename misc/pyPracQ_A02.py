f68d6c9f32d784522f3e4fda26656c9096eee82b
dupadhyaya <dhiru1966@gmail.com>
2019-06-21 17:30:40 +0530

    a

---
+++ misc/pyPracQ_A02.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#Created on Fri Jun 21 10:54:31 2019 : dhiraj@sony
+
+
+#Q
+x=1 ; id(x)
+chr(ord('A'))
+
+L = ['a','b','c','d'] 
+print("".join(L))
+type(type(int))
+y = 8
+z = lambda x : x * y 
+z(6) 
+
+#
+list1 =[3, 4, 5, 20, 5, 25, 1, 3]
+list1.pop(3)
+list1
+
+
+#
+z = 5
+z += 1
+z
+
+#
+a='Python';b='Programming'
+a+b
+a+' '+b
+a-b
+
+#
+a=9.6 ; b=2 ; a//b
+#
+a = True; b = False ;c = True
+a or b and c
+
+#
+abc = 1,000,000;
+a b c = 1000 2000 3000;
+a,b,c = 1000, 2000, 3000;
+a_b_c = 1,000,000
+
+#
+round(0.5) - round(-0.5)
+round(-0.5)
+
+35//4
+25 % 7 
+25/7
+
+import math
+round(math.pi, 2)
+
+divmod(10.5,5)
+divmod(2.4,1.2)
+
+#
+d = {"john":40, "peter":45}
+d["john"]
+
+s1 = set()
+type(s1)
+
+a={1,2,3}
+a.intersection_update({2,3,4,5})
+a
+type(a)
---
+++ misc/pytips_A04.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#Created on Fri Jun 21 11:09:09 2019 : dhiraj@sony
+
+
+#2 inputs from user
+
+x, y = raw_input(),  raw_input() 
+x, y = raw_input().split() 
+x, y = [int(x) for x in raw_input().split()]   
+# Reads two numbers from input and typecasts them to int using  
+# map function 
+x, y = map(int, raw_input().split()) 
+
+
+#Print list vertically
+test_list = [[1, 4, 5], [4, 6, 8], [8, 3, 10]] 
+test_list
+# printing original list 
+print ("The original list is : " + str(test_list)) 
+# using naive method   
+# to print list vertically : like a matrix
+for i in range(len(test_list)): 
+    for x in test_list: 
+        print(x[i], end =' ') 
+    print() 
+    
+# using zip()  # to print list vertically 
+for x, y, z in zip(*test_list): 
+    print(x, y, z) 
+
+#Avoid quotes in list
+# initializing list 
+test_list = ['Dhiraj', 'Upadhyaya', 'Noida'] 
+# printing original list  
+print ("The original list is : " + str(test_list)) 
+# using join() # avoiding printing last comma 
+print ("The formatted output is : " , ' '.join(test_list))
+print(*test_list, sep =', ') 
+
+#Splitting strings
+# using split() + list comprehension 
+  
+# initializing list 
+test_list = [['Python is  the best'], ['All love R and Python'], ['Including me']] 
+test_list  
+# using split() + list comprehension  # Split Sublist Strings 
+res = [sub.split() for subl in test_list for sub in subl] 
+res  
+str(res)
+# using map() + lambda + split()  # Split Sublist Strings 
+res = list(map(lambda sub: sub[0].split(' '), test_list)) 
+str(res)

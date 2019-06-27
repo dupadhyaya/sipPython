b962fc9c8ef12aa4da323ffd8913bb622d9441a1
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-06-27 08:02:07 +0530

    27

--- sip/sipA12_ifelse.py
+++ sip/sipA12_ifelse.py
@@ -4,7 +4,25 @@
 
 
 x=1
+#short methods
 if x == 1 : print("x = 1")
+#
+y=2
+print(x) if x > y else print(y)
+x=1
+print("This is X", x) if x==1 else print("x not equal to 1")
+#3 conditions
+x=3; y=3
+print("X") if x > y else print("=") if x == y else print("Y")
+#longer way
+if x > y:
+    print("X")
+elif x == y:
+    print("=")
+else :
+    print("Y")
+
+
 
 #other logical operations
 #==, !=, <, > , <=, >= 
@@ -17,6 +35,7 @@ if x == 1:
 else:
     print("x not equal to 1")
 x=2
+
 if x == 1: 
     print(" x=1")
 else:
@@ -42,6 +61,4 @@ else :
 
 
 #shorthand if
-x=1
-print("This is X", x) if x==1 else print("x not equal to 1")
 

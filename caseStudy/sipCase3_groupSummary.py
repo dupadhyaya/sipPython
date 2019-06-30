f951c312ca47b97eb6d646ea8f60db5e51358ce1
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-06-30 20:23:30 +0530

    29Jun

--- caseStudy/sipCase3_groupSummary.py
+++ caseStudy/sipCase3_groupSummary.py
@@ -87,7 +87,7 @@ dataGga
 dataGgcc = data2.groupby(['gear','cyl','carb']).mpg.mean()
 dataGgcc
 dataGgcc.index
-dataGgcc.to_clipboard()
+#dataGgcc.to_clipboard()
 
 
 #Unstack after Group
@@ -113,14 +113,12 @@ dataGgcc.loc[4,4,1]  #gear4, cyl4, carb1
 #sort index
 dataGgcc
 #dataGgcc.to_clipboard()
-
 dataGgcc.sort_index()
 #dataGgcc.sort_index().to_clipboard()
-
 dataGgcc.sort_index(level=1)   #cyl
 #dataGgcc.sort_index(level=1).to_clipboard()
 dataGgcc.sort_index(level=2)  #carb
-dataGgcc.sort_index(level=[2,0])  #carb then gear
+dataGgcc.sort_index(level=[2,1])  #carb then gear
 #dataGgcc.sort_index(level=[2,0]).to_clipboard()
 #https://www.geeksforgeeks.org/python-pandas-dataframe-sort_index/
 
@@ -154,3 +152,14 @@ data2.pivot_table(index='gear', values='mpg', columns='cyl')
 pd.pivot_table?
 #table = pivot_table(df, values=['D', 'E'], index=['A', 'C'], aggfunc = {'D': np.mean,'E': [min, max, np.mean]})
 data2.pivot_table(index)
+#s orting DF
+mtcars.sort_values(['mpg','hp']).head(10)
+mtcars.sort_values(['mpg','hp','wt'], ascending=True).head(10)
+mtcars[['gear','cyl','wt']].sort_values(['gear','cyl','wt'], ascending=[True, False, True])
+mtcars.loc[mtcars['mpg'] > 25, ]
+mtcars.loc[(mtcars['mpg'] > 25) & (mtcars['hp'] > 35 ) , ['mpg', 'hp']]
+mtcars.loc[(mtcars['vs'] == 1) | (mtcars['am'] == 1 ) , ['mpg', 'hp','vs','am']].head(10)
+mtcars.pivot_table(index='gear', values='mpg', columns='am')
+mtcars.pivot_table(index=['gear','vs'], values='mpg', columns='am')
+mtcars.pivot_table(index=['gear','vs'], values='mpg', columns=['am','cyl'], fill_value=0)
+mtcars.pivot_table(index=['gear'], columns=['vs','am'],  aggfunc ={'mpg':np.mean,'wt':min})

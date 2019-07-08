89a0bd1550c6f093b738a85e5e0b0e52508b3837
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-07-08 07:57:45 +0530

    d

---
+++ tm/tm_twitter2.py
@@ -0,0 +1,26 @@
+#
+#-----------------------------
+#%
+# -*- coding: utf-8 -*-
+
+#Download tweets from twitter
+import tweepy
+from tweepy import OAuthHandler
+
+#credentials
+consumerKey='uRDuync3BziwQnor1MZFBKp0x'
+consumerSecret='t8QPLr7RKpAg4qa7vth1SBsDvoPKawwwdEhNRjdpY0mfMMdRnV'
+AccessToken='14366551-Fga25zWM1YefkTb2TZYxsrx2LVVSsK0uSpF08sugW'
+AccessTokenSecret='3ap8BZNVoBhE2GaMGLfuvuPF2OrHzM3MhGuPm96p3k6Cz'
+
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(AccessToken, AccessTokenSecret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+keyword = "python"
+
+tweets = api.search(keyword, count=10, lang="en", exclude="retweets", tweet_mode='extended')
+for item in tweets:    print(item)
+
+#data related to tweet, note its datastructure
